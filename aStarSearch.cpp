2dacf67b5a4f5ca685e191dad81c9f10c5b71db3
Adwait Sambare <asambare@nvidia.com>
2018-01-18 16:10:23 +0530

    Partial search algorithm complete

--- aStarSearch.cpp
+++ aStarSearch.cpp
@@ -2,25 +2,44 @@
 #include <set>
 #include <utility>
 #include <math.h>
+#include <stdint.h>
 
 using namespace std;
 
 #define ROW 9
 #define COL 10
+#define FLT_MAX          3.402823466e+38F        // max value
 
 typedef pair<int, int> coordinates;
-typedef pair<int, coordinates> p_pair;
+typedef pair<double, coordinates> p_pair;
 
-class AStartSearch
+struct BlocksDetails
+{
+    double f_score;
+    coordinates parent;
+};
+
+class AStarSearch
 {
 public:
-    AStartSearch() :
+    AStarSearch() :
         mMatrix(NULL),
         mGoal(),
         mSource(),
-        mOpenList()
+        mOpenList(),
+        mClosedList(),
+        mBlocksDetails()
     {
-
+        for(int i = 0; i < ROW; i++)
+        {
+            for(int j = 0; j < COL; j++)
+            {
+                mBlocksDetails[i][j].f_score = FLT_MAX;
+                mBlocksDetails[i][j].parent.first = i;
+                mBlocksDetails[i][j].parent.second = j;
+                mClosedList[i][j] = false;
+            }
+        }
     }
 
     bool search(int matrix[][COL], coordinates source, coordinates goal);
@@ -31,7 +50,7 @@ private:
 
     inline bool isValidBlock(coordinates block)
     {
-        if(block.first >= 0 && block.first <= ROW && block.second >= 0 && block.second <= COL)
+        if(block.first >= 0 && block.first < ROW && block.second >= 0 && block.second < COL)
             return true;
         return false;
     }
@@ -60,7 +79,42 @@ private:
     coordinates mSource;
     set<p_pair> mOpenList;
     bool mClosedList[ROW][COL];
+    BlocksDetails mBlocksDetails[ROW][COL];
 };
+
+bool AStarSearch::search(int matrix[][COL], coordinates source, coordinates goal)
+{
+    if(!isValidBlock(source) || !isValidBlock(goal))
+    {
+        cout<<"Invlaid source or goal\n";
+        return false;
+    }
+
+    mOpenList.insert(make_pair(heuristic(source, goal), make_pair(source.first, source.second)));
+    mBlocksDetails[source.first][source.second] = {heuristic(source, goal), source};
+
+    while(!mOpenList.empty())
+    {
+        p_pair p = *mOpenList.begin();
+        mOpenList.erase(mOpenList.begin());
+        mClosedList[p.second.first][p.second.second] = true;
+
+        //Calculate all the neighbours of p
+
+        //For each neighbor:
+            //If neighbor valid:
+                //if is goal state then return and print path
+                //else
+                    //if in closed list then continue
+                    //else
+                        //calculate f(n) for new neighbor
+                        //Add it to open list
+                        //if f(n) is less than current, update mBlockDetails
+
+        
+
+    }
+}
 
 int main()
 {
