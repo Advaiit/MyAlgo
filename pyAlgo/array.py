13397cf6cc58c9aa068f678123b01a6a18682026
adi1995 <adwait395@gmail.com>
2018-09-05 19:33:32 +0530

    binary search for rotated array added

--- pyAlgo/array.py
+++ pyAlgo/array.py
@@ -52,9 +52,52 @@ def printArray(arr, size):
     
     print('\n')
 
+def binarySearchOnRotated(arr, x, l, r):
+    if l <= r:    
+        m = math.floor(((l + r) / 2))
+        
+        if arr[m] == x:
+            return True
+        
+        if arr[m] > x:
+            if arr[m] > arr[r]:
+                if x == arr[r]:
+                    return True
+                if x > arr[r]:
+                    print("1", m, l, r)
+                    return binarySearchOnRotated(arr, x, l, m-1)
+                else:
+                    print("2", m, l, r)
+                    return binarySearchOnRotated(arr, x, m+1, r)
+            else:
+                print("3", m, l, r)
+                return binarySearchOnRotated(arr, x, l, m-1)
+        else:
+            if arr[m] < arr[l]:
+                if arr[l] == x:
+                    return True
+                if arr[l] < x:
+                    print("4", m, l, r)
+                    return binarySearchOnRotated(arr, x, l, m-1)
+                else:
+                    print("5")
+                    return binarySearchOnRotated(arr, x, m+1, r)
+            else:
+                print("6", m, l, r)
+                return binarySearchOnRotated(arr, x, m+1, r)
+    else:
+        return False
+
+
 if __name__=='__main__':
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     printArray(arr, len(arr))
     #leftRotate(arr, 4)
     reversalLeftRotate(arr, 4)
     printArray(arr, len(arr))
+
+    i = 8
+    print("BinarySearch {} on rotated: ".format(i), binarySearchOnRotated(arr, i, 0, len(arr)-1))
+
+    for i in range(1, 15):
+        print("BinarySearch {} on rotated: ".format(i), binarySearchOnRotated(arr, i, 0, len(arr)-1))
